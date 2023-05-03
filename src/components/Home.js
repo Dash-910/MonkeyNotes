660f85ace959808d4894f6e1d9fdc5301bd37c23
Akash <akashtale910@gmail.com>
2023-05-03 16:05:51 +0530

    Frontend setup Navbar added

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+export const Home =() =>{
+    return (
+        <div>
+            this is home
+        </div>
+    )
+}
